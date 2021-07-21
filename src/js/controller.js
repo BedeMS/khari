521f8008e5bd3dbfed3bbb1273a0b4da72cd3e06
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-07-21 02:56:13 -0400

    Classes formed for Products, just need to create all the classes and remove all the products excess files.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,25 @@
 import navigation from "./views/navigation";
-import header from  "./views/Gender/header";
+import header from "./views/Gender/header";
+import { TShirt } from "./products/productss";
 
 if (module.hot) {
   module.hot.accept();
 }
 
+const tShirts = new TShirt("Crew", [
+  { size: "xl", color: "blue", quantity: 9 },
+  { size: "sm", color: "blue", quantity: 9 },
+]);
+// [{size: "xl", color: "blue",  quantity: 9 }]
+tShirts._addProducts([{ size: "md", color: "blue", quantity: 9 }]);
+tShirts._addImages([
+  { blue: "www.google.com" },
+  { yellow: "www.facebook.com" },
+]);
+tShirts._addProducts([{ size: "xs", color: "red", quantity: 2 }]);
 
-
-
-const init = function(){
-
-}
+const init = function () {
+  console.log(tShirts);
+};
 
 init();
