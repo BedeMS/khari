eef12134bb3fff4bfb82ea567b349a0b2b637f09
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-07-22 02:40:55 -0400

    Created Hoodies, added prices to Classes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,26 @@
 import navigation from "./views/navigation";
 import header from "./views/Gender/header";
-import { TShirt } from "./products/productss";
+import { TShirts } from "./products/products";
+import {comfort, custom, graphic } from "./products/categories/hoodies";
 
 if (module.hot) {
   module.hot.accept();
 }
 
-const tShirts = new TShirt("Crew", [
-  { size: "xl", color: "blue", quantity: 9 },
-  { size: "sm", color: "blue", quantity: 9 },
-]);
-// [{size: "xl", color: "blue",  quantity: 9 }]
-tShirts._addProducts([{ size: "md", color: "blue", quantity: 9 }]);
-tShirts._addImages([
-  { blue: "www.google.com" },
-  { yellow: "www.facebook.com" },
-]);
-tShirts._addProducts([{ size: "xs", color: "red", quantity: 2 }]);
+// const tShirts = new TShirts("Crew", [
+//   { size: "xl", color: "blue", quantity: 9 },
+//   { size: "sm", color: "blue", quantity: 9 },
+// ]);
+// // [{size: "xl", color: "blue",  quantity: 9 }]
+// tShirts._addProducts([{ size: "md", color: "blue", quantity: 9 }]);
+// tShirts._addImages([
+//   { blue: "www.google.com" },
+//   { yellow: "www.facebook.com" },
+// ]);
+// tShirts._addProducts([{ size: "xs", color: "red", quantity: 2 }]);
 
 const init = function () {
-  console.log(tShirts);
+  console.log(comfort, custom, graphic);
 };
 
 init();
