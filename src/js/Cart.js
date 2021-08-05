52288462a5125c0b867fb9b62a24db271eb54f4a
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-08-05 02:22:25 -0400

    Cart.addProduct working to add product, but total is not updated yet and

--- src/js/Cart.js
+++ src/js/Cart.js
@@ -9,15 +9,36 @@ class Cart {
   // 1. Add Product to Cart
   // Product should be an object; = { size: "xs", color: "red", price: 20, etc... }
   addProduct(product) {
-    // if our cart is empty
-    if (!this._cart.length) {
+    // find out if there's a matching product already in the cart;
+    let cartItem = this._findCorrectProduct(product);
+
+    // if there is, just add to that product;
+    if (cartItem.length) {
+      cartItem[0].quantity = product.quantity + cartItem[0].quantity;
+
+      cartItem[0].totalPrice = cartItem[0].quantity * cartItem[0].price;
+
+      this._cart = this._cart.map((item) =>
+        item.id === product.id &&
+        item.size === product.size &&
+        item.color === product.color
+          ? cartItem[0]
+          : item
+      );
+    } else {
       product.totalPrice = product.quantity * product.price;
       this._cart.push(product);
-    } else {
-      // if our cart is not empty, we want to make sure we're not duplicating data.
-      let cartItem = this._findCorrectProduct(product);
+    }
 
-      // If there's a matching product
+    // if our cart is empty
+    // if (!this._cart.length) {
+    //   product.totalPrice = product.quantity * product.price;
+    //   this._cart.push(product);
+    // } else {
+    // if our cart is not empty, we want to make sure we're not duplicating data.
+    // let cartItem = this._findCorrectProduct(product);
+
+    // If there's a matching product
 
     //   if (cartItem.length) {
     //     product.quantity = cartItem[0].quantity + product.quantity;
@@ -31,7 +52,7 @@ class Cart {
     //       ? product
     //       : item
     //   );
-    }
+    // }
 
     localStorage.setItem("cart", JSON.stringify(this._cart));
     return this._cart;
